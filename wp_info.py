b25e67dcbf3a2e7f60ab6f1a02d228fb414c82c7
Sun Liwen <sunliwen@gmail.com>
2013-12-02 14:01:28 +0800

    Cache result and dump to json files.

--- wp_info.py
+++ wp_info.py
@@ -6,6 +6,7 @@ import os
 import re
 import sys
 import wp_query
+import json
 
 class wp_info:
 
@@ -29,27 +30,67 @@ class wp_info:
                 if braces == 0:
                     break
 
+
+def to_json(text):
+    lines = text.split("\n")
+    fields = []
+    for line in lines:
+        if line.startswith("|"):
+            line = line.strip("|").strip()
+            sep_index = line.index("=")
+            key, value = line[:sep_index].strip(), line[sep_index + 1:].strip()
+            fields.append({key: value})
+        elif line.startswith("{{Infobox"):
+            line = line.strip("{{Infobox").strip()
+            fields.append({"infobox": line})
+
+    return fields
+
+
+def load(title):
+    cache = "data/" + title
+    if not os.path.exists(cache):
+        text = wp_query.wp_query(sys.argv[1]).get()
+        print "Saving to cache"
+        f = open(cache, "w")
+        f.write(text)
+    else:
+        with open(cache) as f:
+            print "Loading from cache"
+            text = f.read()
+
+    i = wp_info()
+    i.infobox(text.split("\n"))
+
+    return i.info
+
 # test cases TBD
 #   Aerocar
 #   GitHub
 #   Heroku
-#   Stack Overflow 
+#   Stack Overflow
 
-if __name__=="__main__":
+if __name__ == "__main__":
     if len(sys.argv) == 1:
         print "%s title" % (os.path.basename(__file__))
         exit(1)
     if len(sys.argv) == 2:
-        q = wp_query.wp_query(sys.argv[1])
-        i = wp_info()
-        i.infobox(q.get().split("\n"))
-        if not i.info:
-            # try title case
-            q = wp_query.wp_query(sys.argv[1].title())
-            i.infobox(q.get().split("\n"))
-            print i.info,
-            exit(0)
-        else:
-            print i.info,
-            exit(0)
+        title = sys.argv[1]
+        info = load(title)
+        if not info:
+            title = sys.argv[1].title()
+            info = load(title)
+            if not info:
+                print "Can't find the title online or locally"
 
+        print info,
+
+        json_path = "data/" + title + ".json"
+        with open(json_path, "w") as f:
+            print "Writing to %s" % json_path
+            f.write(json.dumps(to_json(info), indent=2))
+
+        # with open(json_path) as f:
+        #     print f.read()
+
+        exit(0)
